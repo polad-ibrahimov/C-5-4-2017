eb38bd99faf6760435ccda93f4bfc24b6c39eaeb
Polad <polad.ibrahim27@gmail.com>
2017-05-04 17:25:04 +0400

    Add files via upload

---
+++ gelir/Sharpderslerimiz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpderslerimiz
+{
+    class Program
+    {
+        static void Main()
+        {
+            int gun_gelir, gun_sayi, brut_gelir;
+            double faiz, net_gelir;
+
+            Console.WriteLine("Gunluk geliri daxil edin: ");
+            gun_gelir = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Gun sayini daxil edin: ");
+
+            gun_sayi = Convert.ToInt32(Console.ReadLine());
+            brut_gelir = gun_gelir * gun_sayi;
+
+            if(brut_gelir >= 1000)
+            {
+                faiz = brut_gelir * 0.18;
+            }
+            else
+            {
+                faiz = brut_gelir * 0.04;
+            }
+
+            net_gelir = brut_gelir - faiz;
+            Console.WriteLine("Faiz: " + faiz.ToString() + " " + "Net Gelir: " + net_gelir.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ viza/ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int g_viza, g_final;
+
+            double netice;
+
+            Console.WriteLine("viza daxil edin: ");
+            g_viza = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("final daxil edin: ");
+            g_final = Convert.ToInt32(Console.ReadLine());
+
+            netice = g_viza * 0.4 + g_final * 0.6;
+
+            if(netice >= 45)
+            {
+                Console.WriteLine("kecdin");
+            }
+            else
+            {
+                Console.WriteLine("kecmedin");
+            }
+            Console.WriteLine("netice: " + netice.ToString());
+            Console.ReadLine();
+        }
+    }
+}
